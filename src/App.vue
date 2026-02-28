3da31d988ce05d9744e06eac1b6ce615ba753e88
Tom-kkk <32155764@qq.com>
2026-02-28 15:43:13 +0800

    feat:项目初始化

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { VaiButton } from '../packages/components';
+
+</script>
+
+<template>
+  <div class="app">
+    <VaiButton type="primary" @click="() => alert('Clicked!')">Primary Button</VaiButton>
+    <VaiButton type="success" @click="() => alert('Clicked!')">Success Button</VaiButton>
+    <VaiButton disabled @click="() => alert('Clicked!')">Disabled Button</VaiButton>
+    <VaiButton loading @click="() => alert('Clicked!')">Loading Button</VaiButton>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
